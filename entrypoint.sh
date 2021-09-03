f5b736fdf0f6363351338c038728b4dbd76ff858
elmerfdz <elmerfdz@users.noreply.github.com>
2021-09-03 01:09:00 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,38 +37,38 @@ BIND_DATA_DIR=${DATA_DIR}/bind
 WEBMIN_DATA_DIR=${DATA_DIR}/webmin
 
 create_bind_data_dir() {
-  mkdir -p "${BIND_DATA_DIR}"
-  mkdir -p "${BIND_DATA_DIR}"/etc/logs
-  touch "${BIND_DATA_DIR}"/etc/logs/named.log
+  mkdir -p ${BIND_DATA_DIR}
+  mkdir -p ${BIND_DATA_DIR}/etc/logs
+  touch ${BIND_DATA_DIR}/etc/logs/named.log
 
   # populate default bind configuration if it does not exist
-  if [ ! -d "${BIND_DATA_DIR}"/etc ]; then
-    mv /etc/bind "${BIND_DATA_DIR}"/etc
+  if [ ! -d ${BIND_DATA_DIR}/etc ]; then
+    mv /etc/bind ${BIND_DATA_DIR}/etc
   fi
   rm -rf /etc/bind
-  ln -sf "${BIND_DATA_DIR}"/etc /etc/bind
-  chmod -R 0775 "${BIND_DATA_DIR}"
-  chown -R "${BIND_USER}":"${BIND_USER}" "${BIND_DATA_DIR}"
+  ln -sf ${BIND_DATA_DIR}/etc /etc/bind
+  chmod -R 0775 ${BIND_DATA_DIR}
+  chown -R ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}
 
-  if [ ! -d "${BIND_DATA_DIR}"/lib ]; then
-    mkdir -p "${BIND_DATA_DIR}"/lib
-    chown "${BIND_USER}":"${BIND_USER}" "${BIND_DATA_DIR}"/lib
+  if [ ! -d ${BIND_DATA_DIR}/lib ]; then
+    mkdir -p ${BIND_DATA_DIR}/lib
+    chown ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}/lib
   fi
   rm -rf /var/lib/bind
-  ln -sf "${BIND_DATA_DIR}"/lib /var/lib/bind
+  ln -sf ${BIND_DATA_DIR}/lib /var/lib/bind
 }
 
 create_webmin_data_dir() {
-  mkdir -p "${WEBMIN_DATA_DIR}"
-  chmod -R 0755 "${WEBMIN_DATA_DIR}"
-  chown -R root:root "${WEBMIN_DATA_DIR}"
+  mkdir -p ${WEBMIN_DATA_DIR}
+  chmod -R 0755 ${WEBMIN_DATA_DIR}
+  chown -R root:root ${WEBMIN_DATA_DIR}
 
   # populate the default webmin configuration if it does not exist
-  if [ ! -d "${WEBMIN_DATA_DIR}"/etc ]; then
-    mv /etc/webmin "${WEBMIN_DATA_DIR}"/etc
+  if [ ! -d ${WEBMIN_DATA_DIR}/etc ]; then
+    mv /etc/webmin ${WEBMIN_DATA_DIR}/etc
   fi
   rm -rf /etc/webmin
-  ln -sf "${WEBMIN_DATA_DIR}"/etc /etc/webmin
+  ln -sf ${WEBMIN_DATA_DIR}/etc /etc/webmin
 }
 
 disable_webmin_ssl() {
@@ -105,12 +105,12 @@ set_root_passwd() {
 
 create_pid_dir() {
   mkdir -m 0775 -p /var/run/named
-  chown root:"${BIND_USER}" /var/run/named
+  chown root:${BIND_USER} /var/run/named
 }
 
 create_bind_cache_dir() {
   mkdir -m 0775 -p /var/cache/bind
-  chown root:"${BIND_USER}" /var/cache/bind
+  chown root:${BIND_USER} /var/cache/bind
 }
 
 first_init() {
