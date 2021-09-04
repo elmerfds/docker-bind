17bf01069c08a91b5e0cd8324e6317a2852e1f0a
elmerfdz <elmerfdz@users.noreply.github.com>
2021-09-04 18:34:20 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,25 +37,25 @@ BIND_DATA_DIR=${DATA_DIR}/bind
 WEBMIN_DATA_DIR=${DATA_DIR}/webmin
 
 create_bind_data_dir() {
-  mkdir -p "${BIND_DATA_DIR}"
-  mkdir -p "${BIND_DATA_DIR}"/etc/logs
-  touch "${BIND_DATA_DIR}"/etc/logs/named.log
+  mkdir -p ${BIND_DATA_DIR}
+  mkdir -p ${BIND_DATA_DIR}/etc/logs
+  touch ${BIND_DATA_DIR}/etc/logs/named.log
 
   # populate default bind configuration if it does not exist
-  if [ ! -d "${BIND_DATA_DIR}"/etc ]; then
-    mv /etc/bind "${BIND_DATA_DIR}"/etc
+  if [ ! -d ${BIND_DATA_DIR}/etc ]; then
+    mv /etc/bind ${BIND_DATA_DIR}/etc
   fi
-  #rm -rf /etc/bind
-  ln -sf "${BIND_DATA_DIR}"/etc /etc/bind
-  chmod -R 0775 "${BIND_DATA_DIR}"
-  chown -R "${BIND_USER}":"${BIND_USER}" "${BIND_DATA_DIR}"
+  rm -rf /etc/bind
+  ln -sf ${BIND_DATA_DIR}/etc /etc/bind
+  chmod -R 0775 ${BIND_DATA_DIR}
+  chown -R ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}
 
-  if [ ! -d "${BIND_DATA_DIR}"/lib ]; then
-    mkdir -p "${BIND_DATA_DIR}"/lib
-    chown "${BIND_USER}":"${BIND_USER}" "${BIND_DATA_DIR}"/lib
+  if [ ! -d ${BIND_DATA_DIR}/lib ]; then
+    mkdir -p ${BIND_DATA_DIR}/lib
+    chown ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}/lib
   fi
   rm -rf /var/lib/bind
-  ln -sf "${BIND_DATA_DIR}"/lib /var/lib/bind
+  ln -sf ${BIND_DATA_DIR}/lib /var/lib/bind
 }
 
 create_webmin_data_dir() {
